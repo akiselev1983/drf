9bd273910f01f9f16f9b6622b99fd0c30ca111b6
akiselev1983 <akiselev.brd@gmail.com>
2023-06-19 15:26:31 +0300

    add drf lesson1 hw

---
+++ configs/urls.py
@@ -0,0 +1,8 @@
+
+from django.urls import path
+from cars.views import CarListCreateView, CarRetrieveUpdateDestroyView
+
+urlpatterns = [
+    path('cars', CarListCreateView.as_view()),
+path('cars/<int:pk>', CarRetrieveUpdateDestroyView.as_view())
+]
